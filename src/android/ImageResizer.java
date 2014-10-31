56036427cd11e56678a94c679eafd4c3e01616a2
Joschka Schulz <Horstyschulz@gmail.com>
2014-10-31 11:43:59 +0100

    added parameters and bitmap loading from uri

--- src/android/ImageResizer.java
+++ src/android/ImageResizer.java
@@ -1,20 +1,76 @@
 package info.protonet.imageresizer;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 
 public class ImageResizer extends CordovaPlugin {
   public CallbackContext callbackContext;
 
+  private String uri;
+
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    this.callbackContext = callbackContext;
+    try {
+      this.callbackContext = callbackContext;
 
-    Log.i("Protonet", "Dieses Plugin funktioniert :D");
+      Log.i("Protonet", "Dieses Plugin funktioniert :D");
+      Log.i("Protonet", "action: " + action);
+      Log.i("Protonet", "args: " + args);
+      Log.i("Protonet", "callbackContext: " + callbackContext);
 
+      if (action.equals("resize")) {
+        Log.i("Protonet","Resizing all the images.");
+        checkParameters(args);
+        JSONObject jsonObject = args.getJSONObject(0);
+        Log.i("Protonet", "The json Object: " + jsonObject.toString());
+        //Bitmap bitmap = loadBitmapFromUri("");
+        return true;
+      } else {
+        Log.i("Protonet","that's not nice from you... :(");
+        return false;
+      }
+    } catch(JSONException e) {
+    	Log.e("Protonet", "JSON Exception during the Image Resizer Plugin... :(");
+    }
+    return false;
+  }
+
+  private Bitmap loadBitmapFromUri(String uri) {
+    Bitmap resultBitmap = null;
+    InputStream inputStream = null;
+    BufferedInputStream bufferedInputStream = null;
+    try {
+      URLConnection connection = new URL(uri).openConnection();
+      connection.connect();
+      inputStream = connection.getInputStream();
+      bufferedInputStream = new BufferedInputStream(inputStream, 8192);
+      resultBitmap = BitmapFactory.decodeStream(bufferedInputStream);
+    }catch(IOException e) {
+
+    }
+
+    return resultBitmap;
+  }
+
+  private boolean checkParameters(JSONArray args) {
+    if (args.length() != 1) {
+        Log.i("Protonet", "Invalid Number of Arguments");
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
+        return false;
+    }
     return true;
   }
 }
--- www/image_resizer.js
+++ www/image_resizer.js
@@ -1,7 +1,7 @@
 var ImageResizer = function() {};
 
-ImageResizer.prototype.resize = function(success, fail) {
-  cordova.exec(success, fail, "ImageResizer", "resize", []);
+ImageResizer.prototype.resize = function(uri, width, height, success, fail) {
+  cordova.exec(success, fail, "ImageResizer", "resize", [uri, width, height]);
 });
 
 var imageResizer = new ImageResizer();
