bb5e4fe05c2b906cb022d4e96d1fed7df80ec0e4
Joschka Schulz <Horstyschulz@gmail.com>
2014-10-31 17:28:23 +0100

    do a lot of stuff sorry

--- src/android/ImageResizer.java
+++ src/android/ImageResizer.java
@@ -1,73 +1,198 @@
 package info.protonet.imageresizer;
 
+import java.io.FileNotFoundException;
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.FileOutputStream;
+import java.io.File;
 import java.net.URL;
 import java.net.URLConnection;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
+import org.apache.cordova.FileHelper;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.RectF;
 import android.util.Log;
+import android.content.Context;
+import android.provider.MediaStore.Images.Media;
+import android.net.Uri;
+import android.os.Environment;
 
 public class ImageResizer extends CordovaPlugin {
+  private static final int ARGUMENT_NUMBER = 1;
+  private static final String FOLDER_NAME = "Protonet Messenger";
   public CallbackContext callbackContext;
 
   private String uri;
+  private String folderName;
+  private int width;
+  private int height;
 
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     try {
       this.callbackContext = callbackContext;
 
-      Log.i("Protonet", "Dieses Plugin funktioniert :D");
-      Log.i("Protonet", "action: " + action);
-      Log.i("Protonet", "args: " + args);
-      Log.i("Protonet", "callbackContext: " + callbackContext);
-
       if (action.equals("resize")) {
-        Log.i("Protonet","Resizing all the images.");
         checkParameters(args);
+
+        // get the arguments
         JSONObject jsonObject = args.getJSONObject(0);
-        Log.i("Protonet", "The json Object: " + jsonObject.toString());
-        //Bitmap bitmap = loadBitmapFromUri("");
+        uri = jsonObject.getString("uri");
+        folderName = jsonObject.getString("folderName");
+        width = jsonObject.getInt("width");
+        height = jsonObject.getInt("height");
+
+        // load the image from uri
+        Bitmap bitmap = loadScaledBitmapFromUri(uri, width, height);
+
+        // save the image as jpeg on the device
+        Uri scaledFile = saveFile(bitmap);
+
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.OK, scaledFile.toString()));
         return true;
       } else {
-        Log.i("Protonet","that's not nice from you... :(");
+        callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR));
         return false;
       }
     } catch(JSONException e) {
     	Log.e("Protonet", "JSON Exception during the Image Resizer Plugin... :(");
     }
+    callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.ERROR));
     return false;
   }
 
-  private Bitmap loadBitmapFromUri(String uri) {
-    Bitmap resultBitmap = null;
-    InputStream inputStream = null;
-    BufferedInputStream bufferedInputStream = null;
+  /**
+   * Loads a Bitmap of the given android uri path
+   *
+   * @params uri the URI who points to the image
+   **/
+  private Bitmap loadScaledBitmapFromUri(String uriString, int width, int height) {
     try {
-      URLConnection connection = new URL(uri).openConnection();
-      connection.connect();
-      inputStream = connection.getInputStream();
-      bufferedInputStream = new BufferedInputStream(inputStream, 8192);
-      resultBitmap = BitmapFactory.decodeStream(bufferedInputStream);
-    }catch(IOException e) {
+      BitmapFactory.Options options = new BitmapFactory.Options();
+      options.inJustDecodeBounds = true;
+      BitmapFactory.decodeStream(FileHelper.getInputStreamFromUriString(uriString, cordova), null, options);
 
+      //calc aspect ratio
+      int[] retval = calculateAspectRatio(options.outWidth, options.outHeight);
+
+      options.inJustDecodeBounds = false;
+      options.inSampleSize = calculateSampleSize(options.outWidth, options.outHeight, width, height);
+      return BitmapFactory.decodeStream(FileHelper.getInputStreamFromUriString(uriString, cordova), null, options);
+    } catch (FileNotFoundException e) {
+      Log.e("Protonet", "File not found. :(");
+    } catch (IOException e) {
+      Log.e("Protonet", "IO Exception :(");
+    }catch(Exception e) {
+      Log.e("Protonet", e.toString());
+    }
+    return null;
+  }
+
+  private Uri saveFile(Bitmap bitmap) {
+    File folder = new File(Environment.getExternalStorageDirectory() + "/" + FOLDER_NAME);
+    boolean success = true;
+    if (!folder.exists()) {
+      success = folder.mkdir();
     }
 
-    return resultBitmap;
+    if(success) {
+      String fileName = System.currentTimeMillis() + ".jpg";
+      File file = new File(folder, fileName);
+      if(file.exists()) file.delete();
+      try {
+        FileOutputStream out = new FileOutputStream(file);
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+        out.flush();
+        out.close();
+      } catch(Exception e) {
+        Log.e("Protonet", e.toString());
+      }
+      return Uri.fromFile(file);
+    }
+    return null;
+  }
+
+  /**
+   * Figure out what ratio we can load our image into memory at while still being bigger than
+   * our desired width and height
+   *
+   * @param srcWidth
+   * @param srcHeight
+   * @param dstWidth
+   * @param dstHeight
+   * @return
+   */
+  private int calculateSampleSize(int srcWidth, int srcHeight, int dstWidth, int dstHeight) {
+    final float srcAspect = (float)srcWidth / (float)srcHeight;
+    final float dstAspect = (float)dstWidth / (float)dstHeight;
+
+    if (srcAspect > dstAspect) {
+        return srcWidth / dstWidth;
+    } else {
+        return srcHeight / dstHeight;
+    }
+  }
+
+  /**
+   * Maintain the aspect ratio so the resulting image does not look smooshed
+   *
+   * @param origWidth
+   * @param origHeight
+   * @return
+   */
+  private int[] calculateAspectRatio(int origWidth, int origHeight) {
+      int newWidth = width;
+      int newHeight = height;
+
+      // If no new width or height were specified return the original bitmap
+      if (newWidth <= 0 && newHeight <= 0) {
+          newWidth = origWidth;
+          newHeight = origHeight;
+      }
+      // Only the width was specified
+      else if (newWidth > 0 && newHeight <= 0) {
+          newHeight = (newWidth * origHeight) / origWidth;
+      }
+      // only the height was specified
+      else if (newWidth <= 0 && newHeight > 0) {
+          newWidth = (newHeight * origWidth) / origHeight;
+      }
+      // If the user specified both a positive width and height
+      // (potentially different aspect ratio) then the width or height is
+      // scaled so that the image fits while maintaining aspect ratio.
+      // Alternatively, the specified width and height could have been
+      // kept and Bitmap.SCALE_TO_FIT specified when scaling, but this
+      // would result in whitespace in the new image.
+      else {
+          double newRatio = newWidth / (double) newHeight;
+          double origRatio = origWidth / (double) origHeight;
+
+          if (origRatio > newRatio) {
+              newHeight = (newWidth * origHeight) / origWidth;
+          } else if (origRatio < newRatio) {
+              newWidth = (newHeight * origWidth) / origHeight;
+          }
+      }
+
+      int[] retval = new int[2];
+      retval[0] = newWidth;
+      retval[1] = newHeight;
+      return retval;
   }
 
   private boolean checkParameters(JSONArray args) {
-    if (args.length() != 1) {
-        Log.i("Protonet", "Invalid Number of Arguments");
+    if (args.length() != ARGUMENT_NUMBER) {
+        Log.e("Protonet", "Invalid Number of Arguments (" + args.length() + " of " + ARGUMENT_NUMBER + ")");
         callbackContext.sendPluginResult(new PluginResult(PluginResult.Status.INVALID_ACTION));
         return false;
     }
--- www/image_resizer.js
+++ www/image_resizer.js
@@ -1,8 +1,12 @@
 var ImageResizer = function() {};
 
-ImageResizer.prototype.resize = function(uri, width, height, success, fail) {
-  cordova.exec(success, fail, "ImageResizer", "resize", [uri, width, height]);
-});
+ImageResizer.prototype.resize = function(uri, folderName, width, height, success, fail) {
+  cordova.exec(function(uri) {
+    success(uri);
+  }, function() {
+    fail();
+  }, "ImageResizer", "resize", [{uri: uri, folderName: folderName, width: width, height: height}]);
+};
 
 var imageResizer = new ImageResizer();
 module.exports = imageResizer;
