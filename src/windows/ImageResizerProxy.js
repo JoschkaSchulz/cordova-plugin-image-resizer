144c9424f66f383f9624862c15b9b84bb2655325
Ben Marshall <ben3005@hotmail.com>
2016-05-21 12:09:24 +0100

    alternative project reference, wrapping proxy scope

--- src/windows/ImageResizerProxy.js
+++ src/windows/ImageResizerProxy.js
@@ -1,10 +1,15 @@
-﻿cordova.commandProxy.add("ImageResizer",{
-    resize: function (win, fail, args, env) {
-        try {
-            var filePath = ImageResizer.ImageResizer.resize(args);
-            win(filePath);
-        } catch (e) {
-            fail(e);
+﻿(function () {
+    "use strict";
+    var ImageResizerProxy = {
+        resize: function (win, fail, args, env) {
+            try {
+                var filePath = ImageResizer.ImageResizer.resize(args);
+                win(filePath);
+            } catch (e) {
+                fail(e);
+            }
         }
-    }
-});
+    };
+
+    require("cordova/exec/proxy").add("ImageResizer", ImageResizerProxy);
+})();
