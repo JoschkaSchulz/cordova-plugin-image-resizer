635fb22b81c75441ce3d9e1f07bf34e2749ecec0
Snir Segal <snir.segal@gmail.com>
2016-08-09 20:16:58 +0300

    fix image

--- src/ios/ImageResizer.m
+++ src/ios/ImageResizer.m
@@ -5,6 +5,8 @@
 
 #define PROTONET_PHOTO_PREFIX @"protonet_"
 
+static NSInteger count = 0;
+
 @implementation ImageResizer
 
 - (void) resize:(CDVInvokedUrlCommand*)command
@@ -15,39 +17,64 @@
     NSString* quality = [arguments objectForKey:@"quality"];
     CGSize frameSize = CGSizeMake([[arguments objectForKey:@"width"] floatValue], [[arguments objectForKey:@"height"] floatValue]);
     NSString* fileName = [arguments objectForKey:@"fileName"];
-
+    
     //Get the image from the path
     NSURL* imageURL = [NSURL URLWithString:imageUrlString];
     UIImage* sourceImage = [UIImage imageWithData: [NSData dataWithContentsOfURL: imageURL]];
     UIImage *tempImage = nil;
-    CGSize targetSize = frameSize;
-    UIGraphicsBeginImageContext(targetSize);
-
-    CGRect thumbnailRect = CGRectMake(0, 0, 0, 0);
-    thumbnailRect.origin = CGPointMake(0.0,0.0);
-    thumbnailRect.size.width  = targetSize.width;
-    thumbnailRect.size.height = targetSize.height;
-
-    [sourceImage drawInRect:thumbnailRect];
-
+    
+    CGSize imageSize = sourceImage.size;
+    CGFloat width = imageSize.width;
+    CGFloat height = imageSize.height;
+    CGFloat targetWidth = frameSize.width;
+    CGFloat targetHeight = frameSize.height;
+    CGFloat scaleFactor = 0.0;
+    CGSize scaledSize = frameSize;
+    
+    // calculate the aspect ratio if the image is to large
+    if (CGSizeEqualToSize(imageSize, frameSize) == NO) {
+        CGFloat widthFactor = targetWidth / width;
+        CGFloat heightFactor = targetHeight / height;
+        
+        // opposite comparison to imageByScalingAndCroppingForSize in order to contain the image within the given bounds
+        if (widthFactor > heightFactor) {
+            scaleFactor = heightFactor; // scale to fit height
+        } else {
+            scaleFactor = widthFactor; // scale to fit width
+        }
+        scaledSize = CGSizeMake(MIN(width * scaleFactor, targetWidth), MIN(height * scaleFactor, targetHeight));
+    }
+    
+    // If the pixels are floats, it causes a white line in iOS8 and probably other versions too
+    scaledSize.width = (int)scaledSize.width;
+    scaledSize.height = (int)scaledSize.height;
+    
+    UIGraphicsBeginImageContext(scaledSize); // this will resize
+    
+    [sourceImage drawInRect:CGRectMake(0, 0, scaledSize.width, scaledSize.height)];
+    
+    
     tempImage = UIGraphicsGetImageFromCurrentImageContext();
-
+    
     UIGraphicsEndImageContext();
-    NSData *imageData = UIImageJPEGRepresentation(tempImage, 0.1);
+    NSData *imageData = UIImageJPEGRepresentation(tempImage, [quality floatValue] / 100.0f);
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *documentsDirectory = [paths objectAtIndex:0];
-    NSString *imagePath =[documentsDirectory stringByAppendingPathComponent:[NSString stringWithFormat:@"img%ld.jpg",(NSInteger)[[NSDate date]  timeIntervalSince1970]]];
+    NSString *imagePath =[documentsDirectory stringByAppendingPathComponent:[NSString stringWithFormat:@"img%ld.jpg",count]];
+    count++;
     CDVPluginResult* result = nil;
-
+    
     if (![imageData writeToFile:imagePath atomically:NO])
     {
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION messageAsString:@"error save image"];
     }
     else
     {
+        //result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:imagePath];
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[[NSURL fileURLWithPath:imagePath] absoluteString]];
+                  
     }
-
+    
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
 
